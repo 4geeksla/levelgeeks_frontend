d55285a217459581b8a20089f2e5d560009628a4
MagoMarlon <malfonzo@4geeks.co>
2015-11-27 21:59:38 -0430

    Primera estructura generada para el proyecto

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name levelgeeksFrontendApp
+ * @description
+ * # levelgeeksFrontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('levelgeeksFrontendApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name levelgeeksFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the levelgeeksFrontendApp
+ */
+angular.module('levelgeeksFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
